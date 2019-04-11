a3532d7a2cf7f10cb09c4ef288a882d79b0cc0e7
Edem <keltikan@gmail.com>
2019-04-11 03:00:19 +0300

    Controllers added

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,15 @@
+class RoomsController < ApplicationController
+  def index
+    @rooms = Room.all
+    @room = Room.new
+  end
+
+  def show
+    @room = Room.find_by(token: params[:token])
+  end
+
+  def create
+    @room = Room.create!
+    redirect_to @room, notice: 'Room was successfully created.'
+  end
+end
